03ce1a24b7f12dd41535746d1adcef8b1992e22e
Torvald Baade Bringsvor <bringsvor@bringsvor.com>
2014-05-07 10:31:04 +0200

    Now works.

--- Python/KeySMS.py
+++ Python/KeySMS.py
@@ -1,24 +1,12 @@
 
 # KeySMS API client.
-from json import JSONEncoder
+from json import JSONEncoder, JSONDecoder
 from urllib import urlencode
+from urllib2 import urlopen
 
 __author__ = 'Torvald Baade Bringsvor <bringsvor@bringsvor.com>'
 
 # Based upon the Java api.
-"""
-import org.json.simple.*;
-import java.math.BigInteger;
-import java.net.*;
-import java.security.MessageDigest;
-import java.util.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Serializable;
-import java.io.UnsupportedEncodingException;
-"""
 import hashlib
 
 class KeySMS(object):
@@ -35,119 +23,46 @@ class KeySMS(object):
         self.options["scheme"] = "http"
 
     def auth(self, userName, apiKey):
-        """
-        Define what user to auth with. All actions taken will be tied to this user in KeySMS
-        """
+        " Define what user to auth with. All actions taken will be tied to this user in KeySMS "
+
         auth = {"username" : userName,
                 "apiKey" : apiKey}
         self.options['auth'] = auth
 
     def sms(self, message, receivers, date = None, time = None):
-        """
-        Send an SMS some time in the future.
-        """
-        self.message = message
-        response = self.call("/messages", receivers, date, time)
-        return response
+        """ Send an SMS some time in the future (or right now if you don't specify)
+        Returns a dist with the response from the KeySMS service """
 
-    def call(self, inputUrl, receivers, date, time):
-        """
-        Abstracts making HTTP calls
-        """
+        self.message = message
+        response = self._call("/messages", receivers, date, time)
+
+        response_json = JSONDecoder().decode(response[0])
+        return response_json
+
+    def _call(self, inputUrl, receivers, date, time):
+        " Abstracts making HTTP calls "
+
         jsonPayload = JSONEncoder().encode(
             { 'message' : self.message,
             'receivers' : receivers,
             'date' : date,
             'time' : time }
         )
-        # urllib.urlencode
-        signature = ''
-        username = ''
-        url = inputUrl + '?' + urlencode( [('payload', jsonPayload), ('signature', signature), ('username', username)])
-        print "URL", url
-        # https://docs.python.org/2/library/urllib.html?highlight=urlencode#urllib.urlopen
 
-        """
-		JSONArray jsonArray 		= new JSONArray();
-		for (String tlf : recievers)
-		{
-			jsonArray.add(tlf);
-		}
-		JSONObject jsonPayload 		= new JSONObject();
-		jsonPayload.put("message", message);
-		jsonPayload.put("receivers", jsonArray);
-		jsonPayload.put("date", date);
-		jsonPayload.put("time", time);
+        host 				= self.options['host']
+        scheme 				= self.options['scheme']
+        requestURL 			= "%s://%s%s" % (scheme, host, inputUrl)
+        signature		 	= self.sign(jsonPayload)
+        username 			= self.options['auth']['username']
 
-		String host 				= (String) options.get("host");
-		String scheme 				= (String) options.get("scheme");
-		String requestURL 			= scheme + "://" + host + inputUrl;
-		String signature		 	= this.sign(jsonPayload);
-		String username 			= (String)((HashMap)options.get("auth")).get("username");
-		printVariablesToConsole(jsonPayload, host, requestURL, signature, username);
+        data = urlencode( [('payload', jsonPayload),
+                           ('signature', signature),
+                           ('username', username)])
 
-		//Build parameter string
-		String data;
-		try {
-			data = 	"payload="+URLEncoder.encode(jsonPayload.toString(),"UTF-8") +
-					"&signature="+URLEncoder.encode(signature,"UTF-8")			 +
-					"&username="+URLEncoder.encode(username,"UTF-8");
-			System.out.println(data);
-	        try {
+        conn = urlopen(requestURL, data)
+        resp = conn.readlines()
+        return resp
 
-	            // Send the request
-	            URL url = new URL(requestURL);
-	            URLConnection conn = url.openConnection();
-	            conn.setDoOutput(true);
-	            OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
-
-	            //write parameters
-	            writer.write(data);
-	            writer.flush();
-
-	            // Get the response
-	            StringBuffer answer = new StringBuffer();
-	            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-	            String line;
-	            while ((line = reader.readLine()) != null) {
-	                answer.append(line);
-	            }
-	            writer.close();
-	            reader.close();
-
-	            //Return response from server
-	            return answer.toString();
-
-	        } catch (UnsupportedEncodingException e)
-	        {
-	        	e.printStackTrace();
-	        }
-
-        } catch (MalformedURLException ex)
-        {
-            ex.printStackTrace();
-        } catch (IOException ex)
-        {
-            ex.printStackTrace();
-        }
-
-		return null; 		//Returns null on exception.
-	}
-        """
-    """
-	/**
-	 * Print the variables to console to verify their content.
-	 */
-	private void printVariablesToConsole(JSONObject jsonPayload, String host, String requestURL, String signature, String username)
-	{
-		System.out.println("Host: "			+ host);
-		System.out.println("URL " 			+ requestURL);
-		System.out.println("Signature: " 	+  signature);
-		System.out.println("username: " 	+ username);
-		System.out.println("Json Payload: " + jsonPayload);
-		System.out.println();
-	}
-    """
     def sign(self, json):
         """
         Create completely sign string based on payload
@@ -156,8 +71,7 @@ class KeySMS(object):
         @return string Ready to use sign string, just include in request
         """
 
-        #stringToEncode = json.toString() + (String)((HashMap)options.get("auth")).get("apiKey");
-        stringToEncode = json + self.options['auth'] + self.options['apiKey']
+        stringToEncode = json + self.options['auth']['apiKey']
 
         md5engine = hashlib.md5()
         md5engine.update(stringToEncode)
@@ -165,29 +79,3 @@ class KeySMS(object):
         hashtext = ''.join([ '0' for x in range(32-len(md5engine.hexdigest())) ])
         hashtext += md5engine.hexdigest()
         return hashtext
-
-
-
-
-        """
-		try {
-			MessageDigest md5 = java.security.MessageDigest.getInstance("MD5");
-			md5.update(stringToEncode.getBytes("UTF-8"));
-
-			byte[] digest = md5.digest();
-			BigInteger bigInt = new BigInteger(1,digest);
-			String hashtext = bigInt.toString(16);
-			while(hashtext.length() < 32 ){
-			  hashtext = "0"+hashtext;
-			}
-
-			return hashtext;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-
-	}
-    """
-
-
--- Python/example.py
+++ Python/example.py
@@ -1,4 +1,4 @@
-from Python.KeySMS import KeySMS
+from KeySMS import KeySMS
 
 __author__ = 'Torvald Baade Bringsvor <bringsvor@bringsvor.com>'
 
@@ -7,11 +7,14 @@ __author__ = 'Torvald Baade Bringsvor <bringsvor@bringsvor.com>'
 
 if __name__ == '__main__':
     # Instansiate variables
-    userName 	= "98800000"							# Username used to authenicate with
-    apiKey		= "7912390aeeb9db0e2db4f7e4a6430f07"	# API key. Key is obtained through "Min side" at app.keysms.no
-    message 		= "SMS to myself"
-    recievers 	= ["98855555"]
-    # String[] recievers 	= {"98800000", "98800001"};			Could be several receivers :
+
+    userName = "98800000"							# Username used to authenicate with
+    apiKey = "7912390aeeb9db0e2db4f7e4a6430f07"	# API key. Key is obtained through "Min side" at app.keysms.no
+    message	= "SMS to myself"
+
+    receivers = ["98855555"]
+
+    # receivers 	= ["98800000", "98800001"]			Could be several receivers :
     date 		= "2011-03-10"							# If prior to todays date - assumes date == today.
     time			= "16:00"								# Time converted downwards to nearest quarter. eg 14:41 => 14:30
 
@@ -19,5 +22,7 @@ if __name__ == '__main__':
     keySms 		= KeySMS()
     keySms.auth(userName, apiKey)		 									# Define what user to authenticate with.
     # Object response = keySms.sms(message, recievers, date, time); 			// Send SMS to recievers. Returnes response from server. Date and time are optional.
-    response = keySms.sms(message, recievers)
+    response = keySms.sms(message, receivers)
     print(response)
+    assert response['ok'] == True
+
