0f9a20fbbb4330bd99415d57c4aeccdd94c1bcd2
Torvald Baade Bringsvor <bringsvor@bringsvor.com>
2014-05-07 10:54:37 +0200

    Added setup script.

---
+++ Python/setup.py
@@ -0,0 +1,7 @@
+__author__ = 'Torvald Baade Bringsvor <bringsvor@bringsvor.com>'
+
+from distutils.core import setup
+setup(name='KeySMS',
+      version='1.0',
+      py_modules=['KeySMS'],
+      )
